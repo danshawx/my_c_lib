d6432da33a46dff482a0379084e408ef2827618b
Dan Shaw <xiaodanq@126.com>
2020-09-30 20:29:27 +0800

    Create my_task.h

---
+++ my_task.h
@@ -0,0 +1,21 @@
+#ifndef MY_TASK_H_
+#define MY_TASK_H_
+
+#include "stdint.h"
+
+typedef void(*my_task_runnable_t)(void);
+
+typedef struct
+{
+    my_task_runnable_t runnable;
+    uint32_t           periodicTimeMs;
+    uint32_t           remainToExec;
+} My_Task_t;
+
+
+#define SCHED_MAX_TASKS    1
+
+void My_Task_Init(void);
+void My_Sched_Loop(void);
+
+#endif
