a1128f137b3db41d88465b47eba992d83e789bb6
Dan Shaw <xiaodanq@126.com>
2020-09-30 20:28:48 +0800

    Create my_task.c

---
+++ my_task.c
@@ -0,0 +1,25 @@
+
+#include "my_task.h"
+
+My_Task_t g_uMy_Task[SCHED_MAX_TASKS] =
+{
+    {Comm_Task, 100, 100},
+};
+
+
+void My_Sched_Loop(void)
+{
+    uint8_t i;
+
+    for (i = 0; i < SCHED_MAX_TASKS; i++)
+    {
+        if(HAL_GetTick() >= g_uMy_Task[i].remainToExec)
+        {
+            g_uMy_Task[i].runnable();
+            g_uMy_Task[i].remainToExec = g_uMy_Task[i].periodicTimeMs + HAL_GetTick();
+
+            //Iwdg_Clear();
+        }
+    }
+    //exp deal
+}
